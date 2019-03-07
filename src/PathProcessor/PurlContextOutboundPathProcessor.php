4b2447a9fd4010a2e34f17f4f62ff2d40bdddd69
mpetrovic <mpetrovic@guneva.net>
2019-03-07 15:59:20 -0500

    Handle purl_contexts that are integer ids. openscholar/openscholar#11046

--- src/PathProcessor/PurlContextOutboundPathProcessor.php
+++ src/PathProcessor/PurlContextOutboundPathProcessor.php
@@ -7,6 +7,8 @@ use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\purl\Context;
 use Drupal\purl\ContextHelper;
 use Drupal\purl\MatchedModifiers;
+use Drupal\purl\Modifier;
+use Drupal\purl\Plugin\ModifierIndex;
 use Symfony\Component\HttpFoundation\Request;
 
 class PurlContextOutboundPathProcessor implements OutboundPathProcessorInterface
@@ -26,31 +28,53 @@ class PurlContextOutboundPathProcessor implements OutboundPathProcessorInterface
       $this->contextHelper = $contextHelper;
     }
 
-    public function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL)
-    {
-      if (count($this->matchedModifiers->getMatched()) && $bubbleable_metadata) {
-        $cacheContexts = $bubbleable_metadata->getCacheContexts();
-        $cacheContexts[] = 'purl';
-        $bubbleable_metadata->setCacheContexts($cacheContexts);
-      }
-
-        if (array_key_exists('purl_context', $options) && $options['purl_context'] == false) {
-
+    public function processOutbound($path, &$options = array(), Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
+      if (array_key_exists('purl_context', $options)) {
+        // purl has been set manually
+        if (isset($options['purl_context']['id'])) {
+          /** @var Modifier[] $modifiers */
+          if ($modifiers = \Drupal::service('purl.modifier_index')->getModifiersById($options['purl_context']['id'])) {
+            $mod = reset($modifiers); // TODO: Some way to make a provider preferred so we can make sure custom domains always take priority
+            if ($bubbleable_metadata) {
+              $bubbleable_metadata->setCacheContexts(['purl:' . $options['purl_context']['id']]);
+            }
             return $this->contextHelper->processOutbound(
-              $this->matchedModifiers->createContexts(Context::EXIT_CONTEXT),
+              array_merge($this->matchedModifiers->createContexts(Context::EXIT_CONTEXT), [new Context($mod->getModifierKey(), $mod->getMethod())]),
               $path,
               $options,
               $request,
               $bubbleable_metadata
             );
+          }
         }
+        // forced out of the purl
+        if ($options['purl_context'] === FALSE) {
+          if ($bubbleable_metadata) {
+            $bubbleable_metadata->setCacheContexts(['purl:none']);
+          }
+          return $this->contextHelper->processOutbound(
+            $this->matchedModifiers->createContexts(Context::EXIT_CONTEXT),
+            $path,
+            $options,
+            $request,
+            $bubbleable_metadata
+          );
+        }
+      }
 
-        return $this->contextHelper->processOutbound(
-          $this->matchedModifiers->createContexts(),
-          $path,
-          $options,
-          $request,
-          $bubbleable_metadata
-        );
+      // check if path already has a purl in it.
+
+      if (count($this->matchedModifiers->getMatched()) && $bubbleable_metadata) {
+        $cacheContexts = $bubbleable_metadata->getCacheContexts();
+        $cacheContexts[] = 'purl';
+        $bubbleable_metadata->setCacheContexts($cacheContexts);
+      }
+      return $this->contextHelper->processOutbound(
+        $this->matchedModifiers->createContexts(),
+        $path,
+        $options,
+        $request,
+        $bubbleable_metadata
+      );
     }
 }
